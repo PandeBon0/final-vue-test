44aa139f370cccb95e6041593d5cfe3b2ceb56a6
Davy Jem <dscastanedav@unal.edu.co>
2022-09-19 14:24:50 -0500

    responsive design almost finished

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class=" bg-bgGrey ">
+  <div >
 
     <router-view/>
  
--- src/components/TheStatistics.vue
+++ src/components/TheStatistics.vue
@@ -2,10 +2,15 @@
 
 <div class="flex flex-col
   justify-evenly items-center text-center
-bg-white border-2 border-dbGreyBorder mt-4 mx-12 font-openSans h-88">
+bg-white border-2 border-dbGreyBorder  mt-4 mx-12 font-openSans h-120 sm:h-107 md:h-105  lg:h-88">
 
-  <div class="grid grid-rows-1 grid-cols-2 justify-between space-x-100 -mt-32">
-    <h2 class="font-bold text-lg text-left ">Estadísticas</h2>
+  <div class="grid grid-cols-1 text-center grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 sm:justify-between 
+    sm:space-x-0 md:space-x-48 lg:space-x-64 xl:space-x-100 lg:-mt-32
+   md:-mt-36  
+   -mt-12
+   sm:-mt-32
+   ">
+    <h2 class="font-bold text-lg sm:text-left ">Estadísticas</h2>
     <div class="border-2 rounded border-slate-60 shadow-md text-sm p-0.5 mx-8 justify-end items-end">
       <label 
         for="tiempo" 
@@ -22,43 +27,49 @@ bg-white border-2 border-dbGreyBorder mt-4 mx-12 font-openSans h-88">
   </div>
 
          
-  <div class="flex text-left ml-20 h-20 -mt-32">
+  <div class=" 
+    grid grid-cols-1
+    sm:grid-cols-2  
+    lg:flex text-left ml-20 
+    -mt-48
+    md:-mt-28
+    lg:h-20 lg:-mt-32   ">
 
-  <div class="grid grid-rows-2 ">
+  <div class="grid grid-rows-2 md:mt-0 mt-32  ">
     <div class="grid grid-cols-2">
       <p>Visitas</p>
       <img 
-        class="mx-3 mt-0 w-128 h-5 my-12 object-scale-down -ml-28" 
+        class="mx-3 mt-0 w-128 h-5 my-12 object-scale-down -ml-28 invisible lg:visible  " 
         src="@/assets/StatisticsImages/Trazado1015.png"
       >
     </div>
     <BarChart />
   </div> 
-  <div class="grid grid-rows-2 ">
+  <div class="grid grid-rows-2 md:mt-0 mt-32 ">
     <div class="grid grid-cols-2">
       <p>Conversiones</p>
       <img 
-        class="mx-3 mt-0 w-128 h-5 my-12 object-scale-down -ml-20" 
+        class="mx-3 mt-0 w-128 h-5 my-12 object-scale-down  lg:-ml-20 invisible lg:visible " 
         src="@/assets/StatisticsImages/Trazado1015.png"
       >
     </div>
     <BarChart />
   </div> 
-  <div class="grid grid-rows-2 ">
+  <div class="grid grid-rows-2 mt-32 lg:mt-0  ">
     <div class="grid grid-cols-2">
       <p>Taza de conversión</p>
       <img 
-        class="mx-3 mt-0 w-128 h-5 my-12 object-scale-down -ml-14" 
+        class="mx-3  mt-0 w-128 h-5 my-12 object-scale-down -ml-14 invisible lg:visible" 
         src="@/assets/StatisticsImages/Trazado1015.png"
       >
     </div>
     <BarChart />
   </div> 
-  <div class="grid grid-rows-2 ">
+  <div class="grid grid-rows-2 mt-32 lg:mt-0  ">
     <div class="grid grid-cols-2">
       <p>Ventas</p>
       <img 
-        class="mx-3 mt-0 w-128 h-5 my-12 object-scale-down -ml-28" 
+        class="mx-3  mt-0 w-128 h-5 my-12 object-scale-down -ml-28 invisible lg:visible" 
         src="@/assets/StatisticsImages/Trazado1015.png"
       >
     </div>
