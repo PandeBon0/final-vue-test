51fa1d92f0ef480d99e49d23d80ec2a7fba957c8
Davy Jem <dscastanedav@unal.edu.co>
2022-10-06 01:47:22 -0500

    file input added and working properly

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,11 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-  
-import { defineComponent } from 'vue';
-// import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    // HelloWorld
-  }
-});
-=======
 export default {
   name: 'App',
   components: {
 }
 }
->>>>>>> 339beb4e13860a484c033804596974d32c97df41
 </script>
 
 <style scoped>
